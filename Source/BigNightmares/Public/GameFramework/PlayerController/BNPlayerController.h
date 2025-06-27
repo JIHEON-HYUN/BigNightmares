419ef3b7277d47df3f56462ff2dae52f397804a3
JH.H <74130565+JIHEON-HYUN@users.noreply.github.com>
2025-06-27 16:16:38 +0900

    GameFramework Initialize

---
+++ Source/BigNightmares/Public/GameFramework/PlayerController/BNPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "BNPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BIGNIGHTMARES_API ABNPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
