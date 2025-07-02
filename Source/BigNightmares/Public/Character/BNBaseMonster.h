309955e27862db62e02af144ce5dc1541e48ba3c
Chance <aday031@naver.com>
2025-07-02 09:43:08 +0900

    Init BNBaseMonster

---
+++ Source/BigNightmares/Public/Character/BNBaseMonster.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/BNBaseCharacter.h"
+#include "BNBaseMonster.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BIGNIGHTMARES_API ABNBaseMonster : public ABNBaseCharacter
+{
+	GENERATED_BODY()
+	
+};
