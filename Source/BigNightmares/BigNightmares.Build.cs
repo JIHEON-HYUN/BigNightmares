a017a7a504469ccc8a2af4604813c2154b0a810d
Chance <aday031@naver.com>
2025-07-04 14:56:38 +0900

    Temp

--- Source/BigNightmares/BigNightmares.Build.cs
+++ Source/BigNightmares/BigNightmares.Build.cs
@@ -10,9 +10,9 @@ public class BigNightmares : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[]
 		{
-			"Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput"
-			,"GameplayAbilities", "GameplayTags", "GameplayTasks", "AnimGraphRuntime",
-			"OnlineSubsystem", "AIModule"
+			"Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "GameplayAbilities", 
+			"GameplayTags", "GameplayTasks", "AnimGraphRuntime", "OnlineSubsystem", "OnlineSubsystemUtils", 
+			"OnlineSubsystemSteam", "AIModule"
 		});
 
 		PrivateDependencyModuleNames.AddRange(new string[]
